9d431b8209baa0d9b45723d98bd3ac66c7b6f903
gyokuro06 <gyokuro06@gmail.com>
2023-05-04 18:30:53 +0900

    create project for learning how to switch attribute

---
+++ attribute-binding/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang='ts'>
+import SwitchColorMessage from './components/SwitchColorMessage.vue';
+import { ref } from 'vue';
+
+const switchRedMessageColor = ref('')
+const switchGreenAndBlueMessageColor = ref('green')
+
+const switchRedOrNot = () => {
+  switchRedMessageColor.value = switchRedMessageColor.value === ''
+    ? 'red'
+    : ''
+}
+
+const switchGreenOrBlue = () => {
+  switchGreenAndBlueMessageColor.value = switchGreenAndBlueMessageColor.value === 'green'
+    ? 'blue'
+    : 'green'
+}
+</script>
+
+<template>
+  <SwitchColorMessage :color="switchRedMessageColor" @switch-color="switchRedOrNot">
+    This should be red... but click me to toggle it
+  </SwitchColorMessage>
+  <SwitchColorMessage :color="switchGreenAndBlueMessageColor" @switch-color="switchGreenOrBlue">
+    This should be green, and should toggle between green and blue on click.
+  </SwitchColorMessage>
+</template>
+
+<style scoped></style>
---
+++ attribute-binding/src/components/SwitchColorMessage.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+import { computed } from 'vue'
+
+const props = defineProps(['color'])
+const emits = defineEmits(['switch-color'])
+
+const colorStyle = computed(() => {
+  return {
+    color: props.color ?? ''
+  }
+})
+
+</script>
+
+<template>
+  <p class="switch-color-message" :style="colorStyle" @click="emits('switch-color')">
+    <slot />
+  </p>
+</template>
+
+<style scoped></style>

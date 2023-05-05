b5a9359755639a81154d04c14f1052c38b1e4a15
gyokuro06 <gyokuro06@gmail.com>
2023-05-06 05:29:45 +0900

    create project for learning how to dom manipulating with condition and loop

---
+++ conditional-and-loops/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+import Button from './components/Button.vue'
+
+const show = ref(true)
+const list = ref([1, 2, 3])
+
+const pushItem = () => {
+  list.value.push(list.value.length + 1)
+}
+const popItem = () => {
+  list.value.pop()
+}
+const reverseList = () => {
+  list.value.reverse()
+}
+</script>
+
+<template>
+  <div class="buttons-container">
+    <Button @click="show = !show">Toggle List</Button>
+    <Button @click="pushItem">Push Number</Button>
+    <Button @click="popItem">Pop Number</Button>
+    <Button @click="reverseList">Reverse List</Button>
+    <main>
+      <ul v-if="show && list.length">
+        <li v-for="item of list">{{ item }}</li>
+      </ul>
+      <p v-else-if="list.length">List is not empty, but hidden.</p>
+      <p v-else>List is empty.</p>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+Button {
+  margin: 0 8px 8px 0;
+}
+</style>
